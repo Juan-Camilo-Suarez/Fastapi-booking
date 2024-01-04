10a08e8f02d2ebbfef7380acbe48a4d2ab09d4ee
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-04 15:47:04 +0300

    + fix class setting in config

--- app/database.py
+++ app/database.py
@@ -1,23 +1,7 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
-
-    @property
-    def DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    model_config = SettingsConfigDict(env_file=".env")
-
-
-settings = Settings()
+from app.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL)
 
