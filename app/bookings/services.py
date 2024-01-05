966d03778dac6c6c1945515246c4353cf98f39b7
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-05 17:12:11 +0300

    + redifine logic for services

--- app/bookings/services.py
+++ app/bookings/services.py
@@ -1,14 +1,8 @@
 from sqlalchemy import select
 
 from app.bookings.models import Bookings
-from app.database import async_session_maker
+from app.services.base import BaseServices
 
 
-class BookingService:
-    # classmehtod para no tener que crear una instacia cada vez que se quiera usar el metodo
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(Bookings.__table__.columns)
-            result = await session.execute(query)
-            return result.mappings().all()
+class BookingService(BaseServices):
+    model = Bookings
