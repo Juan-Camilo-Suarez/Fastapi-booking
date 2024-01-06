c4b2c41a1b870b2c395ca49e20da4e219a941e55
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-06 17:46:41 +0300

    + fix logic to vefy user

--- app/users/auth.py
+++ app/users/auth.py
@@ -31,6 +31,5 @@ def create_access_token(data: dict) -> str:
 
 async def authenticate_user(email: EmailStr, password: str):
     user = await UserServices.find_one_or_none(email=email)
-    if not user and not verify_password(password, user.hashed_password):
-        return None
-    return user
+    if user and verify_password(password, user.hashed_password):
+        return user
