2aa3a0ef7bb856537943f1677a5e61281ce0f92f
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-07 10:17:22 +0300

    + logout endpoint

--- app/users/router.py
+++ app/users/router.py
@@ -36,3 +36,8 @@ async def login_user(response: Response, user_data: UserAuthDTO):
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return {"access_token": access_token}
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("booking_access_token")
