56e8b89b66788e819066c80eac91f7042dfd3fe6
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-07 13:43:41 +0300

    + created endpoint to booking

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,7 +1,9 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends
 
-from app.bookings.booking_dto import BookingDTO
 from app.bookings.services import BookingService
+from app.exceptions import RoomCannotBeBookedException
 from app.users.dependecies import get_current_user
 from app.users.models import Users
 
@@ -16,7 +18,9 @@ router = APIRouter(
 async def get_bookings(user: Users = Depends(get_current_user)):
     return await BookingService.find_all(user_id=user.id)
 
-@router.post('')
-async def add_bookings(user: Users = Depends(get_current_user)):
-    await BookingService.add(user_id=user.id)
 
+@router.post('')
+async def add_bookings(room_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
+    booking = await BookingService.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBookedException
