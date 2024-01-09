049a9ccd36a3ce93d8f12fd87dce9d91c2b72590
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-09 21:07:19 +0300

    + added in main routers for

--- app/main.py
+++ app/main.py
@@ -4,9 +4,13 @@ from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_hotels_rooms
+from app.pages.router import router as router_pages
 
 app = FastAPI()
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_hotels_rooms)
+
+# this routeres for frontend
+app.include_router(router_pages)
