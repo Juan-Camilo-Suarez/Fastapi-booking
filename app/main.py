c2b57fbdfbc7d6bf99e9ec2c8fd2beebc910b249
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-01-09 21:15:43 +0300

    + create a mount app to get pictures

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
@@ -7,6 +8,9 @@ from app.hotels.rooms.router import router as router_hotels_rooms
 from app.pages.router import router as router_pages
 
 app = FastAPI()
+# endpoint to get avaliable pictures(this create independent app)
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
+
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
