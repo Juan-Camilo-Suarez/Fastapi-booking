178b068bad82b9e2c4a5892ea5e5cb7eb8488bc7
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-02-03 18:51:19 +0300

    implemented tasks on router

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,9 +1,12 @@
 from datetime import date
 
 from fastapi import APIRouter, Depends
+from pydantic import parse_obj_as
 
+from app.bookings.booking_dto import BookingDTO
 from app.bookings.services import BookingService
 from app.exceptions import RoomCannotBeBookedException
+from app.tasks.tasks import send_booking_confirmation_email
 from app.users.dependecies import get_current_user
 from app.users.models import Users
 
@@ -22,5 +25,7 @@ async def get_bookings(user: Users = Depends(get_current_user)):
 @router.post('')
 async def add_bookings(room_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
     booking = await BookingService.add(user.id, room_id, date_from, date_to)
+    booking_dict = {'date_from': date_from}
+    send_booking_confirmation_email.delay(booking_dict, user.email)
     if not booking:
         raise RoomCannotBeBookedException
