75a872cbe96c27ee606e688f5ee7e9ab62b30d6f
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2024-02-03 17:00:44 +0300

    + use varibles from venv

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from fastapi_cache.decorator import cache
 from redis import asyncio as aioredis
 
 from app.bookings.router import router as router_bookings
+from app.config import settings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_hotels_rooms
@@ -52,5 +53,5 @@ async def index():
 
 @app.on_event("startup")
 async def startup():
-    redis = aioredis.from_url("redis://localhost:6379")
+    redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}")
     FastAPICache.init(RedisBackend(redis), prefix="cache")
